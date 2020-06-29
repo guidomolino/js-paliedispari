f09ab130fb01b5f61e225f9c4d095baa10e1ce4d
Guido <molido@hotmail.it>
2020-06-29 17:03:07 +0200

    Secondo Commit

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,20 +1,50 @@
 
 var parola = prompt("Inserisci una parola");
+var parolaPalindroma = palindrome(parola);
+
+
+if (parolaPalindroma) {
+  console.log("La parola è palindroma");
+}else {
+  console.log("La parola non è palindroma");
+}
+
+// funzione
+
+function palindrome(str) {
+
+    var len = str.length;
+    var mid = Math.floor(len/2);
+
+    for ( var i = 0; i < mid; i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// senza funzione
+
+// var parolaArray = parola.split("");
+// console.log(parolaArray);
 
 // palindroma:
 // come faccio a vedere che è uguale nel verso opposto?
-var palindroma = false;
 
-for (var i = 0; i < parola.length; i++) {
-  if (parola[i] == parola[parola.lenght - i]) {
-    palindroma = true;
-  }else {
-    palindroma = false;
-  }
-}
-
-if (palindroma = true) {
-  console.log("la parola è palindroma");
-}else if (palindroma = false) {
-  console.log("la parola non è palindroma");
-}
+// var palindroma = true;
+// var i = 0;
+//
+// while (palindroma == true && i != parolaArray.lenght) {
+  //   if (parolaArray[i] != parolaArray[parolaArray.length - i]) {
+    //     palindroma = false;
+    //   }
+      //   i++
+      // };
+      //
+      // if (palindroma == true) {
+        //   console.log("la parola è palindroma");
+        // }else if (palindroma == false) {
+          //   console.log("la parola non è palindroma");
+          // };
